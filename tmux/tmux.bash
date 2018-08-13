3a8646acad7379406d94b6b26d94e1383dca837b
Steven Feldman <steven.feldman.cc>
2018-08-13 04:39:37 +0000

    Added switch to tmux shortcuts

--- tmux/tmux.bash
+++ tmux/tmux.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Wrapper arround tmux to handle common shortcuts
-# [ ls | ( a|attach <name> )
+# [ ls | ( a|attach <name> ) | (s|switch <name>) ]
 
 
 if [ "$1" = "ls" ]; then
@@ -13,8 +13,8 @@ if [ "$1" = "attach" ] || [ "$1" = "a" ]; then
   exit
 fi
 
-if [ "$1" = "save" ] || [ "$1" = "s" ]; then
-  echo "save is not implemented."
+if [ "$1" = "switch" ] || [ "$1" = "s" ]; then
+  exec tmux switch -t $2
   exit
 fi
 
