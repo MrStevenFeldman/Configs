d7107b60efe3aab144b8dc75cb0acb8a0f05c7e5
Steven Feldman <steven.feldman.cc>
2018-08-13 05:50:25 +0000

    adding vimrc

--- setup.sh
+++ setup.sh
@@ -5,6 +5,12 @@
 
 ln -sf $PWD/bash/bash_aliases ~/.bash_aliases
 ln -sf $PWD/bash/bashrc ~/.bashrc
+
 ln -sf $PWD/tmux/tmux.conf ~/.tmux.conf
 mkdir -p ~/.tmux/plugins
 ln -sf $PWD/tmux/tpm ~/.tmux/plugins/tpm
+
+mkdir -p ~/.vim/bundle
+ln -sf $PWD/vim/Vundle.vim ~/.vim/bundle/Vundle.vim
+ln -sf $PWD/vim/.vimrc ~/.vimrc
+
