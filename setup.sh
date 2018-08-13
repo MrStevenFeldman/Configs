56773713a88ed31bc90f28056e21c521f1b5e922
Steven Feldman <steven.feldman.cc>
2018-08-13 17:45:58 +0000

    cleanup and vim symlinks

--- setup.sh
+++ setup.sh
@@ -1,16 +1,20 @@
-# TODO: this script should do the following
-# - check that i3, tmux, vim, etc... are installed
-# - set symlink to this directory
-# - 
+# This file handles the setup of my linux dot files.
 
-ln -sf $PWD/bash/bash_aliases ~/.bash_aliases
+# Shell files.
 ln -sf $PWD/bash/bashrc ~/.bashrc
+ln -sf $PWD/bash/bash_aliases ~/.bash_aliases
 
+# TMUX files.
+# TODO(check tmux is installed)
 ln -sf $PWD/tmux/tmux.conf ~/.tmux.conf
 mkdir -p ~/.tmux/plugins
 ln -sf $PWD/tmux/tpm ~/.tmux/plugins/tpm
 
-mkdir -p ~/.vim/bundle
-ln -sf $PWD/vim/Vundle.vim ~/.vim/bundle/Vundle.vim
-ln -sf $PWD/vim/.vimrc ~/.vimrc
+# VIM files.
+# TODO(check vim is installed)
+# https://github.com/amix/vimrc#how-to-install-the-awesome-version
+ln -sf $PWD/vim/vimrc ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
 
+# Window Manager (I3)
+# TODO: Add i3 support.
