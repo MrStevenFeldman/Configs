334aba414852d76441e6b1648f256f1defcf4021
Steven Feldman <steven.feldman.cc>
2018-08-13 02:24:01 +0000

    add symlinks to setup code

--- setup.sh
+++ setup.sh
@@ -2,3 +2,7 @@
 # - check that i3, tmux, vim, etc... are installed
 # - set symlink to this directory
 # - 
+
+ln -sf $PWD/bash/bash_aliases ~/.bash_aliases
+ln -sf $PWD/bash/bashrc ~/.bashrc
+ln -sf $PWD/tmux/tmux.conf ~/.tmux.conf
